6f57392366a43a4a8c36df42747702807b8854b6
Jessica <duellje@oregonstate.edu>
2020-08-10 14:40:10 -0400

    added get endpoints

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -2,4 +2,40 @@ const express = require('express')
 const db = require('../data/dbConfig')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  db('accounts')
+    .select('*')
+    .then(accounts => {
+      res.status(200).json({ data: accounts })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ errorMessage: err.message })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const accountId = req.params.id
+  db('accounts')
+    .where({ id: accountId })
+    .select('*')
+    .then(account => {
+      if (account) {
+        res.status(200).json({ data: account })
+      } else {
+        res.status(404).json({ message: 'Account with that ID not found' })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      req.status(500).json({ errorMessage: err.message })
+    })
+})
+
+router.post('/:id', (req, res) => {})
+
+router.put('/:id', (req, res) => {})
+
+router.delete('/:id', (req, res) => {})
+
 module.exports = router
