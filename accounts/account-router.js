7ad9348da57a49066af9338d2a9e5c53d0525b18
Jessica <duellje@oregonstate.edu>
2020-08-11 15:55:08 -0400

    minor change

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -1,94 +1,94 @@
-const express = require('express')
-const db = require('../data/dbConfig')
-const router = express.Router()
+const express = require("express");
+const db = require("../data/dbConfig");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  const queryString = req.body
+router.get("/", (req, res) => {
+  const queryString = req.body;
 
-  db('accounts')
-    .select('*')
+  db("accounts")
+    .select("*")
     .limit(queryString.limit || 100)
-    .orderBy((queryString.sortby, queryString.sortdir) || 'id')
-    .then(accounts => {
-      res.status(200).json({ data: accounts })
+    .orderBy((queryString.sortby, queryString.sortdir) || "id")
+    .then((accounts) => {
+      res.status(200).json({ data: accounts });
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ errorMessage: err.message })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
 
-router.get('/:id', (req, res) => {
-  const accountId = req.params.id
-  db('accounts')
+router.get("/:id", (req, res) => {
+  const accountId = req.params.id;
+  db("accounts")
     .where({ id: accountId })
-    .select('*')
-    .then(account => {
+    .select("*")
+    .then((account) => {
       if (account) {
-        res.status(200).json({ data: account })
+        res.status(200).json({ data: account });
       } else {
-        res.status(404).json({ message: 'Account with that ID not found' })
+        res.status(404).json({ message: "Account with that ID not found" });
       }
     })
-    .catch(err => {
-      console.log(err)
-      req.status(500).json({ errorMessage: err.message })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      req.status(500).json({ errorMessage: err.message });
+    });
+});
 
-router.post('/:id', (req, res) => {
-  const account = req.body
-  const accountId = req.params.id
+router.post("/", (req, res) => {
+  const account = req.body;
+  const accountId = req.params.id;
 
-  db('accounts')
+  db("accounts")
     .insert(account)
-    .returning('account')
-    .then(accounts => {
-      res.status(201).json({ inserted: accounts })
+    .returning("account")
+    .then((accounts) => {
+      res.status(201).json({ inserted: accounts });
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ errorMessage: err.message })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
 
-router.put('/:id', (req, res) => {
-  const accountId = req.params.id
-  const changes = req.body
+router.put("/:id", (req, res) => {
+  const accountId = req.params.id;
+  const changes = req.body;
 
-  db('accounts')
+  db("accounts")
     .where({ id: accountId })
     .update(changes)
-    .then(count => {
+    .then((count) => {
       if (count) {
-        res.status(200).json({ message: 'Updated account successfully' })
+        res.status(200).json({ message: "Updated account successfully" });
       } else {
-        res.status(404).json({ message: 'Account with that ID not found' })
+        res.status(404).json({ message: "Account with that ID not found" });
       }
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ errorMessage: err.message })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
 
-router.delete('/:id', (req, res) => {
-  const accountId = req.params.id
+router.delete("/:id", (req, res) => {
+  const accountId = req.params.id;
 
-  db('accounts')
+  db("accounts")
     .where({ id: accountId })
     .delete()
-    .then(count => {
+    .then((count) => {
       if (count) {
-        res.status(200).json({ message: 'Account deleted successfully' })
+        res.status(200).json({ message: "Account deleted successfully" });
       } else {
-        res.status(404).json({ message: 'Account with that ID not found' })
+        res.status(404).json({ message: "Account with that ID not found" });
       }
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ errorMessage: err.message })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
 
-module.exports = router
+module.exports = router;
